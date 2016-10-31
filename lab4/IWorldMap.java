d4b4ac9f8dcc35b7af65ef711552a586fed88f2d
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2016-10-31 02:17:31 +0100

    Zmiana opisu lab 4 + dodanie definicji klas

---
+++ lab4/IWorldMap.java
@@ -0,0 +1,59 @@
+package agh.cs.lab4;
+
+import agh.cs.lab2.MoveDirection;
+import agh.cs.lab2.Position;
+
+/**
+ * Interface responsible for interacting with the map of the world.
+ * 
+ * @author apohllo
+ *
+ */
+public interface IWorldMap {
+	/**
+	 * Indicates if any object can move to the given position.
+	 * 
+	 * @param position
+	 *            The position checked for the movement possibility.
+	 * @return True if the object can move to that position.
+	 */
+	boolean canMoveTo(Position position);
+
+	/**
+	 * Add a car the map.
+	 * 
+	 * @param car
+	 *            The car to add.
+	 * @return True if the car was added.
+	 */
+	boolean add(Car car);
+
+	/**
+	 * Move the cars on the map according to the provided move directions. Every
+	 * n-th direction should be sent the n-th car on the map.
+	 * 
+	 * @param directions
+	 *            Array of move directions.
+	 */
+	void run(MoveDirection[] directions);
+
+	/**
+	 * Returns true if given position on the map is occupied. Should not be
+	 * confused with canMove since there might be empty positions where the car
+	 * cannot move.
+	 * 
+	 * @param position
+	 *            Position to check.
+	 * @return True if the position is occupied.
+	 */
+	boolean isOccupied(Position position);
+
+	/**
+	 * Return object at given position.
+	 * 
+	 * @param position
+	 *            The position of the object.
+	 * @return Object or null if the position is not occupied.
+	 */
+	Object objectAt(Position position);
+}
---
+++ lab4/MapVisualizer.java
@@ -0,0 +1,64 @@
+package agh.cs.lab4;
+
+import agh.cs.lab2.Position;
+import agh.cs.lab4.IWorldMap;
+
+/**
+ * The map visualizer converts the {@link IWorldMap} map into a string
+ * representation.
+ * 
+ * @author apohllo
+ *
+ */
+public class MapVisualizer {
+	/**
+	 * Convert selected region of the map into a string. It is assumed that the
+	 * indices of the map will have no more than two characters (including the
+	 * sign).
+	 * 
+	 * @param map
+	 *            The map to convert.
+	 * @param lowerLeft
+	 *            The lower left corner of the region that is dumped.
+	 * @param upperRight
+	 *            The upper right corner of the region that is dumped.
+	 * @return String representation of the selected region of the map.
+	 */
+	public String dump(IWorldMap map, Position lowerLeft, Position upperRight) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = upperRight.y + 1; i >= lowerLeft.y - 1; i--) {
+			if (i == upperRight.y + 1) {
+				builder.append(" y\\x ");
+				for (int j = lowerLeft.y; j < upperRight.x + 1; j++) {
+					builder.append(String.format("%2d", j));
+				}
+				builder.append(System.lineSeparator());
+			}
+			builder.append(String.format("%3d: ", i));
+			for (int j = lowerLeft.y; j <= upperRight.x + 1; j++) {
+				Position currentPosition = new Position(j, i);
+				if (j <= upperRight.x) {
+					if (i < lowerLeft.y || i > upperRight.y) {
+						builder.append("--");
+					} else {
+						builder.append("|");
+						if (map.isOccupied(currentPosition)) {
+							Object object = map.objectAt(currentPosition);
+							builder.append(object.toString());
+						} else {
+							builder.append(" ");
+						}
+					}
+				} else {
+					if (i < lowerLeft.y || i > upperRight.y) {
+						builder.append("-");
+					} else {
+						builder.append("|");
+					}
+				}
+			}
+			builder.append(System.lineSeparator());
+		}
+		return builder.toString();
+	}
+}
